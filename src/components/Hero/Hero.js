223158c34348650634e98f096a5d472ada5b6c7d
Mike Chi <mikechi@reclaimmikechi.lan>
2021-06-01 23:31:49 -0700

    adding in circuit images

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
 import './Hero.scss'
+import circuit_left from '../../assets/hero/Circut-Left.png';
+import circuit_right from '../../assets/hero/Circut-Right.png';
 
 const Hero = () => {
     return(
         <section className="hero">
             {/* <canvas></canvas> */}
-            <div className="hero__circle-left"></div>
-            <div className="hero__circle-right"></div>
+            <div className="hero__circle-left">
+                <img src={circuit_left} className="hero__circuit-left-img" aria-hidden="true"/>
+            </div>
+            
+            <div className="hero__circle-right">
+                <img src={circuit_right} className="hero__circuit-right-img" aria-hidden="true"/>
+            </div>
             <div className="hero__content-container">
                 <h1 className="hero__title">
                     Protecting your ideas should be simple

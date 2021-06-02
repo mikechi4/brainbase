95aad8d78947faad79feec885abe3cdebe08e12d
Mike Chi <mikechi@reclaimmikechi.lan>
2021-06-01 22:22:53 -0700

    initial hero setup

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './Hero.scss'
+
+const Hero = () => {
+    return(
+        <section className="hero">
+            <div className="hero__circle-left"></div>
+            <div className="hero__content-container">
+                <h1 className="hero__title">
+                    Protecting your ideas should be simple
+                </h1>
+                <p className="hero__copy">
+                    Brainbase is a modern, accessible platform for all your intellectual property needs. From filing your very first trademark to protecting and monetizing your IP, we make it simple every step of the way. 
+                </p>
+            </div>
+        </section>
+    )
+};
+
+export default Hero;

89b81f0e325f9b5e0a85a5b4a388df51878d5919
Mike Chi <mikechi@reclaimmikechi.lan>
2021-06-02 00:26:39 -0700

    finished footer code

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './reset.css';
 
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
         <Navbar />    
-        <Hero/>
+        <Hero />
+        <Footer />
     </div>
   );
 }
